3d4fba737359845ac2e349c221fdd787074594ba
Chris Sherlock <chris@lida.com.au>
2012-04-02 23:26:33 +1000

    Cleanup - remove extraneous functions, etc.

--- jsSMControls.js
+++ jsSMControls.js
@@ -1,112 +1,94 @@
 (function() {
+	
+	/* Session Management functions (login, logout) */
+	
+	// Setup session manager namespace
+	window['SessionManager'] = {};
+	
+	// private variables
+	var session;
+	
+	// Public functions
+	function Login(username, password) {
+		var html;
+		
+		// turn off caching
+		$.ajaxSetup ({  
+		    cache: false  
+		}); 
+		
+		$.ajax({
+			url:"http://chris-pc/ita/ServiceManager.aspx?&TemplateName=InLogin&USER_ID=" + username + "&PASS_WORD=" + password + "&DATABASE=1", 
+			success:function(result) {
+				html = $(result);
+				session = $("#ID", html).attr("value");
+				//alert(session);
+				if ( $("#ID").length === 0 ) { // ID value doesn't exist
+					$("body").append("<input type=\"hidden\" id=\"ID\"" + "value = \"" + session + "\">"); 
+				}
+				//alert("Logged in!");
+			}
+		});
+	}
+
+	window['SessionManager']['Login'] = Login;
+	
+	function GetSession() {
+		return session;
+	}
+	
+	window['SessionManager']['GetSession'] = GetSession;
+	
+	function Logout() {
+		alert(session);
+		$.post("http://chris-pc/ita/ServiceManager.aspx?BTN_EXIT=True", 
+			{ ID: GetSession() },
+			function(data) {
+				alert("Logged out");
+			});
+	}
+	
+	window['SessionManager']['Logout'] = Logout;
+
+	/* Database controls functions (login, logout) 
+	   NOTE: this does NOT work in Internet Explorer. The reason for this is that 
+	         Internet Explorer assumes that all string data is null terminated, whereas
+			 ECMA-262 section 8.4 merely states:
+			     
+				 "The String type is the set of all finite ordered sequences of zero or 
+				  more 16-bit unsigned integer values (?elements?). The String type is 
+				  generally used to represent textual data in a running ECMAScript program, 
+				  in which case each element in the String is treated as a code unit value 
+				  (see Clause 6). Each element is regarded as occupying a position within 
+				  the sequence. These positions are indexed with nonnegative integers. The 
+				  first element (if any) is at position 0, the next element (if any) at 
+				  position 1, and so on. The length of a String is the number of elements 
+				  (i.e., 16-bit values) within it. The empty String has length zero and 
+				  therefore contains no elements.
+				  
+				  When a String contains actual textual data, each element is considered to 
+				  be a single UTF-16 code unit. Whether or not this is the actual storage format 
+				  of a String, the characters within a String are numbered by their initial code 
+				  unit element position as though they were represented using UTF-16. All 
+				  operations on Strings (except as otherwise stated) treat them as sequences of 
+				  undifferentiated 16-bit unsigned integers; they do not ensure the resulting 
+				  String is in normalised form, nor do they ensure language-sensitive results."
+				  
+			(see also section 4.3.16)
+			
+			There IS a hack to get around this via using some sort of CStr function and extracting 
+			the information from IE's reponsebody header, but until I can work out a clean way of 
+			resolving this code problem, IE is not supported. 
+			
+			Code patches welcome :-)
+	   */
+	
+	// Setup database controls namespace
+	
 	window['DBControl'] = {};
 	
 	var dbControl;
-	var session;
 	var data;
-	
-	// Get around brain damaged problem with Internet Explorer and not being able to access XMLHttpRequest.responseBody.  
-	// See http://miskun.com/javascript/internet-explorer-and-binary-files-data-access/ and also
-	// http://stackoverflow.com/questions/1919972/how-do-i-access-xhr-responsebody-for-binary-data-from-javascript-in-ie
-	
-	if(/msie/i.test(navigator.userAgent) && !/opera/i.test(navigator.userAgent)) {
-		var IEBinaryToArray_ByteStr_Script =
-		"<!-- IEBinaryToArray_ByteStr -->\r\n"+
-		"<script type='text/vbscript'>\r\n"+
-		"Function IEBinaryToArray_ByteStr(Binary)\r\n"+
-		"   IEBinaryToArray_ByteStr = CStr(Binary)\r\n"+
-		"End Function\r\n"+
-		"Function IEBinaryToArray_ByteStr_Last(Binary)\r\n"+
-		"   Dim lastIndex\r\n"+
-		"   lastIndex = LenB(Binary)\r\n"+
-		"   if lastIndex mod 2 Then\r\n"+
-		"       IEBinaryToArray_ByteStr_Last = Chr( AscB( MidB( Binary, lastIndex, 1 ) ) )\r\n"+
-		"   Else\r\n"+
-		"       IEBinaryToArray_ByteStr_Last = "+'""'+"\r\n"+
-		"   End If\r\n"+
-		"End Function\r\n"+
-		"</script>\r\n";
-
-		// inject VBScript
-		document.write(IEBinaryToArray_ByteStr_Script);
-	}
-	
-	function getXMLHttpRequest() 
-	{
-		if (window.XMLHttpRequest) {
-			return new window.XMLHttpRequest;
-		}
-		else {
-			try {
-				return new ActiveXObject("MSXML2.XMLHTTP"); 
-			}
-			catch(ex) {
-				return null;
-			}
-		}
-	}
-
-	
-	function AjaxQuery(fileURL)
-	{
-		if(!/msie/i.test(navigator.userAgent)) {
-			$.ajax({
-				url:fileURL,
-				async: false,			
-				success:function(result) {
-					data = result;
-				}
-			});
-		} else {
-			IeBinFileReaderImpl(fileURL);
-		}
-	}
-	
-	function IeBinFileReaderImpl(fileURL) {
-		this.req = getXMLHttpRequest();
-		this.req.open("GET", fileURL, true);
-		this.req.setRequestHeader("Accept-Charset", "x-user-defined");
-		// my helper to convert from responseBody to a "responseText" like thing
-		var convertResponseBodyToText = function (binary) {
-			var byteMapping = {};
-			for ( var i = 0; i < 256; i++ ) {
-				for ( var j = 0; j < 256; j++ ) {
-					byteMapping[ String.fromCharCode( i + j * 256 ) ] =
-						String.fromCharCode(i) + String.fromCharCode(j);
-				}
-			}
-			// call into VBScript utility fns
-			var rawBytes = IEBinaryToArray_ByteStr(binary);
-			var lastChr = IEBinaryToArray_ByteStr_Last(binary);
-			return rawBytes.replace(/[\s\S]/g,
-									function( match ) { return byteMapping[match]; }) + lastChr;
-		};
-
-		this.req.onreadystatechange = function(event){
-			if (that.req.readyState == 4) {
-				that.status = "Status: " + that.req.status;
-				//that.httpStatus = that.req.status;
-				if (that.req.status == 200) {
-					// this doesn't work
-					//fileContents = that.req.responseBody.toArray(); 
-
-					// this doesn't work
-					//fileContents = new VBArray(that.req.responseBody).toArray(); 
-
-					// this works...
-					var fileContents = convertResponseBodyToText(that.req.responseBody);
-
-					fileSize = fileContents.length-1;
-					if(that.fileSize < 0) throwException(_exception.FileLoadFailed);
-					that.readByteAt = function(i){
-						return fileContents.charCodeAt(i) & 0xff;
-					};
-				}
-				if (typeof callback == "function"){ callback(that);}
-			}
-		};
-		this.req.send();
-	}
 	
 	function GetEnvVariables(data) {
 		var stringData = data.toString();
@@ -237,120 +219,26 @@
 	
 	window['DBControl']['ParseData'] = ParseData;
 	
-	function getDBControl() {
-		var elements = document.getElementsByTagName("object");
-		
-		for (var i = 0; i < elements.length; i++) {
-			var tag = elements[i];
-			var isDB = tag.getAttribute("classid") == "CLSID:5C98EC99-4964-4290-A14D-FF4B9D4E8696" ? true: false;  
-						
-			if (isDB) {
-				return tag;
-			}
-		} 
-		
-		return null;	
-	}
-	
-	function clearParam() {
-		dbControl = dbControl || getDBControl();
-		if (dbControl != null) {
-			dbControl.clearParam();
-			return true;
-		}
-		return false;
-	}
-	
-	window['DBControl']['clearParam'] = clearParam;
-	
-	function AddParam(param, value) {
-		dbControl = dbControl || getDBControl();
-		if (dbControl != null) {
-			if (param == null || value == null) {
-				return false;
-			} else {
-				dbControl.AddParam(param, value);
-				return true;
-			}
-		}
-	}
-	
-	window['DBControl']['AddParam'] = AddParam;
-	
 	function GetRecordSet(query) {
-		dbControl = dbControl || getDBControl();
-		if (dbControl != null) {
-			if (query == null) {
-				return null;
-			} else {
-				return dbControl.GetRecordSet(query);
-			}
-		}
-	}
-	
-	window['DBControl']['GetRecordSet'] = GetRecordSet;
-	
-	function GetRecordSet2(query) {
-		var getRSUrl = "http://chris-pc/ita/ServiceManager.aspx?GetRS&ID=" + session + "&Query=" + query; 
+		// FIXME - needs the ability to add parameters
+		var getRSUrl = "/ServiceManager.aspx?GetRS&ID=" + session + "&Query=" + query; 
 		
-		$.ajaxSetup ({  
-		    cache: false  
-		}); 
-		
-		AjaxQuery(getRSUrl);		
-		
-		return data;
-	}
-	
-	window['DBControl']['GetRecordSet2'] = GetRecordSet2; 
-	
-	window['SessionManager'] = {};
-	
-	// Login
-	
-	function Login(username, password) {
-		var html;
-		
-		// turn off caching
 		$.ajaxSetup ({  
 		    cache: false  
 		}); 
 		
 		$.ajax({
-			url:"http://chris-pc/ita/ServiceManager.aspx?&TemplateName=InLogin&USER_ID=" + username + "&PASS_WORD=" + password + "&DATABASE=1", 
+			url:fileURL,
+			async: false,			
 			success:function(result) {
-				html = $(result);
-				session = $("#ID", html).attr("value");
-				//alert(session);
-				if ( $("#ID").length === 0 ) { // ID value doesn't exist
-					$("body").append("<input type=\"hidden\" id=\"ID\"" + "value = \"" + session + "\">"); 
-				}
-				//alert("Logged in!");
+				data = result;
 			}
-		});
-	}
-
-	window['SessionManager']['Login'] = Login;
-	
-	function GetSession() {
-		return session;
+		});		
+		
+		return data;
 	}
 	
-	window['SessionManager']['GetSession'] = GetSession;
-	
-	// Logout
-	
-	function Logout() {
-		alert(session);
-		$.post("http://chris-pc/ita/ServiceManager.aspx?BTN_EXIT=True", 
-			{ ID: GetSession() },
-			function(data) {
-				alert("Logged out");
-			});
-	}
-	
-	window['SessionManager']['Logout'] = Logout;
-	
+	window['DBControl']['GetRecordSet'] = GetRecordSet; 
 	
 })();
 	
