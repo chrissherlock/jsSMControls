9122f49c63f6f2422ed8f38c21b37e398add93f8
Chris Sherlock <chris@lida.com.au>
2012-04-10 00:16:56 +1000

    Hook up login click event

--- misc/equivActualControls/DB.js
+++ misc/equivActualControls/DB.js
@@ -1,8 +1,14 @@
 $(document).ready(function() { 
+	var sm = new SessionManager();
+	
 	$("#Test").click(function() {
 		var DB = $("#DB");
 		DB.clearParam();
 		DB.addParam("NAME", "(GMT-10:00) Hawaii");
 		DB.getRecordSet("Select TIME_ZONE");
+	});
+	
+	$("#Login").click(function() { 
+		sm.Login($('#USER_ID').val(), $('#PASSWORD').val());
 	});
 })
