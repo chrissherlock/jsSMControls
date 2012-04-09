1897ff64a995f67c3df2be6842b28e63f8a8223c
Chris Sherlock <chris@lida.com.au>
2012-04-10 00:09:08 +1000

    add controls info

---
+++ misc/equivActualControls/DB.js
@@ -0,0 +1,8 @@
+$(document).ready(function() { 
+	$("#Test").click(function() {
+		var DB = $("#DB");
+		DB.clearParam();
+		DB.addParam("NAME", "(GMT-10:00) Hawaii");
+		DB.getRecordSet("Select TIME_ZONE");
+	};
+});
